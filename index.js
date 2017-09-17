5f522fed83ca701021273e9135ef129e9eba5445
SiegMysen <arlethvann@hotmail.com>
2017-09-17 09:55:39 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const commando = require('discord.js-commando');
+const path = require('path');
+
+const client = new commando.Client({
+    commandPrefix: '~',
+    owner: '//Owner ID goes here',
+    disableEveryone: true
+});
+
+
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['util', 'Utility Commands'],
+        ['info', 'Info Commands'],
+        ['fun', 'Silly Commands']
+
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+
+client.on('ready', () => {
+    console.log('Logged in!');
+    client.user.setGame('Best Waifu');
+});
+
+client.login('//Bot Login token goes here')
